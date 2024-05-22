9ae5e77366b4d4060145b167929885848596de68
DLOzTOR <dloztor.aar@outlook.com>
2024-05-22 10:32:44 +0300

    gulp file include

--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,38 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const { watch, series } = require("gulp");
+const fileinclude = require('gulp-file-include');
 const pagesScssLocation = "./assets/scss/pages/*.scss";
 const scssLocation = "./assets/scss/**/*.scss";
 const scssBuildLocation = "./assets/css";
-function buildStyles() {
+const pathsHtml = {
+    src: './pages/',
+    dest: './build/'
+};
+async function buildStyles() {
   return gulp.src(pagesScssLocation)
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(gulp.dest(scssBuildLocation));
 };
+async function buildHtml(){
+  return gulp.src([
+    pathsHtml.src + '*.html'
+    ])
+    .pipe(fileinclude({
+      prefix: '@@',
+      basepath: '@file'
+    }))
+    .pipe(gulp.dest(pathsHtml.dest));
+}
 
-exports.build = buildStyles;
+async function build(){
+  buildStyles();
+  buildHtml();
+}
 
-exports.watch = function () {
+exports.build = build;
+
+exports.watch = async function () {
     watch(scssLocation, buildStyles);
+    watch(pathsHtml.src, buildHtml);
 }
